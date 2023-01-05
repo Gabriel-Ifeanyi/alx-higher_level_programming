ba568d2ed8e8ea424d7a56bd00883d06be128fcd
Zeelgabriels <zealgabrles@gmail.com>
2023-01-04 16:03:33 -0800

    Task_0. Run Python file

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,6 +3,14 @@
 
 #include <stdlib.h>
 
+/**
+ * struct listint_s - Would do the singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 
 typedef struct listint_s
 {
