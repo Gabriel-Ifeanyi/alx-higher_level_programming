8e6079efd5fe4de0e74160229cbc577a6a9280f2
Zeelgabriels <zealgabrles@gmail.com>
2023-01-31 17:58:20 -0800

    All 0x07. Python - Test-driven development tasks

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,10 +1,11 @@
+/*
  * File: 102-python.c
  */
 
 #include "Python.h"
 
 /**
- * print_python_string - Prints information about Python strings.
+ * print_python_string - Would print information about Python strings.
  * @p: A PyObject string object.
  */
 void print_python_string(PyObject *p)
