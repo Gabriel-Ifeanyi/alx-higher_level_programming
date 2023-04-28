dfab6296c06f67828b394c6ebfc7959a04a7f139
Zeelgabriels <zealgabrles@gmail.com>
2023-04-27 18:42:57 -0700

    Task_5. cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Would get the response body from a POST request to a given URL
-curl -sX "$1" POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+# Get the response body from a POST request to a given URL
+curl -sX "$1" POST -d "email=test@gmail.com&subject=I will always be here for PLD"01~
